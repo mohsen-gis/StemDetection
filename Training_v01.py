6ff346e9ecf41ee21a47872248aec88aa4d6e292
joanmanbar <35076815+joanmanbar@users.noreply.github.com>
2021-05-08 00:11:59 -0500

    Training... (test)
    Training... (test)

---
+++ Training_v01.py
@@ -0,0 +1,256 @@
+# -*- coding: utf-8 -*-
+
+
+
+# Training stardist
+
+
+
+
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+import sys
+import numpy as np
+import matplotlib
+# matplotlib.rcParams["image.interpolation"] = None
+import matplotlib.pyplot as plt
+# %matplotlib inline
+# %config InlineBackend.figure_format = 'retina'
+
+from glob import glob
+from tqdm import tqdm
+from tifffile import imread
+from csbdeep.utils import Path, normalize
+
+from stardist import fill_label_holes, random_label_cmap, calculate_extents, gputools_available
+from stardist.matching import matching, matching_dataset
+from stardist.models import Config2D, StarDist2D, StarDistData2D
+
+
+
+# Random label cmap
+import matplotlib
+import colorsys
+def random_label_cmap(n=2**16):
+
+    h,l,s = np.random.uniform(0,1,n), 0.4 + np.random.uniform(0,0.6,n), 0.2 + np.random.uniform(0,0.8,n)
+    cols = np.stack([colorsys.hls_to_rgb(_h,_l,_s) for _h,_l,_s in zip(h,l,s)],axis=0)
+    cols[0] = 0
+    return matplotlib.colors.ListedColormap(cols)
+
+lbl_cmap = random_label_cmap()
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Set path for images and labels
+X = sorted(glob('ground_truth/images/*.tif'))
+Y = sorted(glob('ground_truth/masks/*.tif'))
+assert all(Path(x).name==Path(y).name for x,y in zip(X,Y))
+
+# Load sample image
+X, Y = X[:10], Y[:10]
+
+X = list(map(imread,X))
+Y = list(map(imread,Y))
+n_channel = 1 if X[0].ndim == 2 else X[0].shape[-1]
+
+
+
+
+
+
+# NORMALIZE
+# This may not be necessary in our case
+
+axis_norm = (0,1)   # normalize channels independently
+# axis_norm = (0,1,2) # normalize channels jointly
+if n_channel > 1:
+    print("Normalizing image channels %s." % ('jointly' if axis_norm is None or 2 in axis_norm else 'independently'))
+    sys.stdout.flush()
+
+X = [normalize(x,1,99.8,axis=axis_norm) for x in tqdm(X)]
+Y = [fill_label_holes(y) for y in tqdm(Y)]
+
+
+
+
+
+# Split into train and validation datasets.
+
+assert len(X) > 1, "not enough training data"
+rng = np.random.RandomState(42)
+ind = rng.permutation(len(X))
+n_val = max(1, int(round(0.15 * len(ind))))
+ind_train, ind_val = ind[:-n_val], ind[-n_val:]
+X_val, Y_val = [X[i] for i in ind_val]  , [Y[i] for i in ind_val]
+X_trn, Y_trn = [X[i] for i in ind_train], [Y[i] for i in ind_train] 
+print('number of images: %3d' % len(X))
+print('- training:       %3d' % len(X_trn))
+print('- validation:     %3d' % len(X_val))
+
+
+def plot_img_label(img, lbl, img_title="image", lbl_title="label", **kwargs):
+    fig, (ai,al) = plt.subplots(1,2, figsize=(12,5), gridspec_kw=dict(width_ratios=(1.25,1)))
+    im = ai.imshow(img, cmap='gray', clim=(0,1))
+    ai.set_title(img_title)    
+    fig.colorbar(im, ax=ai)
+    al.imshow(lbl, cmap=lbl_cmap)
+    al.set_title(lbl_title)
+    plt.tight_layout()
+
+# Example image
+i = min(9, len(X)-1)
+img, lbl = X[i], Y[i]
+assert img.ndim in (2,3)
+img = img if (img.ndim==2 or img.shape[-1]==3) else img[...,0]
+plot_img_label(img,lbl)
+# None;
+
+
+
+
+
+# Configuration
+
+# A StarDist2D model is specified via a Config2D object.
+print(Config2D.__doc__)
+
+
+
+
+# 32 is a good default choice (see 1_data.ipynb)
+n_rays = 32
+
+# Use OpenCL-based computations for data generator during training (requires 'gputools')
+use_gpu = False and gputools_available()
+
+# Predict on subsampled grid for increased efficiency and larger field of view
+grid = (2,2)
+
+conf = Config2D (
+    n_rays       = n_rays,
+    grid         = grid,
+    use_gpu      = use_gpu,
+    n_channel_in = n_channel,
+)
+print(conf)
+vars(conf)
+
+
+if use_gpu:
+    from csbdeep.utils.tf import limit_gpu_memory
+    # adjust as necessary: limit GPU memory to be used by TensorFlow to leave some to OpenCL-based computations
+    limit_gpu_memory(0.8)
+    # alternatively, try this:
+    # limit_gpu_memory(None, allow_growth=True)
+
+
+# Note: The trained StarDist2D model will not predict completed shapes for partially visible objects at the image boundary if train_shape_completion=False (which is the default option).
+
+# We could start with train_shape_completion=False and change it later to seek improvement
+
+model = StarDist2D(conf, name='stardist', basedir='models')
+
+# Check if the neural network has a large enough field of view to see up to the boundary of most objects.
+median_size = calculate_extents(list(Y), np.median)
+fov = np.array(model._axes_tile_overlap('YX'))
+print(f"median object size:      {median_size}")
+print(f"network field of view :  {fov}")
+if any(median_size > fov):
+    print("WARNING: median object size larger than field of view of the neural network.")
+
+
+
+
+
+# Data Augmentation
+
+def random_fliprot(img, mask): 
+    assert img.ndim >= mask.ndim
+    axes = tuple(range(mask.ndim))
+    perm = tuple(np.random.permutation(axes))
+    img = img.transpose(perm + tuple(range(mask.ndim, img.ndim))) 
+    mask = mask.transpose(perm) 
+    for ax in axes: 
+        if np.random.rand() > 0.5:
+            img = np.flip(img, axis=ax)
+            mask = np.flip(mask, axis=ax)
+    return img, mask 
+
+def random_intensity_change(img):
+    img = img*np.random.uniform(0.6,2) + np.random.uniform(-0.2,0.2)
+    return img
+
+
+def augmenter(x, y):
+    """Augmentation of a single input/label image pair.
+    x is an input image
+    y is the corresponding ground-truth label image
+    """
+    x, y = random_fliprot(x, y)
+    x = random_intensity_change(x)
+    # add some gaussian noise
+    sig = 0.02*np.random.uniform(0,1)
+    x = x + sig*np.random.normal(0,1,x.shape)
+    return x, y
+
+
+
+# plot some augmented examples
+img, lbl = X[0],Y[0]
+plot_img_label(img, lbl)
+for _ in range(3):
+    img_aug, lbl_aug = augmenter(img,lbl)
+    plot_img_label(img_aug, lbl_aug, img_title="image augmented", lbl_title="label augmented")
+
+
+
+
+
+
+
+
+# Training
+
+# We recommend to monitor the progress during training with TensorBoard. You can start it in the shell from the current working directory like this:
+
+# I ran this on my PC's command prompt, then open the browser and typed http://localhost:6006/ (Joan)
+# python -m tensorboard.main --logdir=. --port 6006
+
+
+
+quick_demo = False
+
+if quick_demo:
+    print (
+        "NOTE: This is only for a quick demonstration!\n"
+        "      Please set the variable 'quick_demo = False' for proper (long) training.",
+        file=sys.stderr, flush=True
+    )
+    model.train(X_trn, Y_trn, validation_data=(X_val,Y_val), augmenter=augmenter,
+                epochs=2, steps_per_epoch=10)
+
+    print("====> Stopping training and loading previously trained demo model from disk.", file=sys.stderr, flush=True)
+    model = StarDist2D.from_pretrained('2D_demo')
+else:
+    model.train(X_trn, Y_trn, validation_data=(X_val,Y_val), augmenter=augmenter)
+None;
+
+
+
+
+if quick_demo:
+    model.optimize_thresholds(X_val[:2], Y_val[:2])
+else:
+    model.optimize_thresholds(X_val, Y_val)
+
--- Verify_Images_Imported.py
+++ Verify_Images_Imported.py
@@ -1,74 +1,74 @@
-# # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 
-# # Veifying that images get imported correctly
-
-
-
+# Veifying that images get imported correctly
 
 
 
 
 
 
-# from __future__ import print_function, unicode_literals, absolute_import, division
-# import numpy as np
-# import matplotlib
-# # matplotlib.rcParams["image.interpolation"] = None
-# import matplotlib.pyplot as plt
-# # %matplotlib inline
-# # %config InlineBackend.figure_format = 'retina'
-
-# from glob import glob
-# from tqdm import tqdm
-# from tifffile import imread
-# from csbdeep.utils import Path, download_and_extract_zip_file
-
-# from stardist import fill_label_holes, relabel_image_stardist, random_label_cmap
-# from stardist.matching import matching_dataset
 
 
 
-# # Random label cmap
-# import matplotlib
-# import colorsys
-# def random_label_cmap(n=2**16):
+from __future__ import print_function, unicode_literals, absolute_import, division
+import numpy as np
+import matplotlib
+# matplotlib.rcParams["image.interpolation"] = None
+import matplotlib.pyplot as plt
+# %matplotlib inline
+# %config InlineBackend.figure_format = 'retina'
 
-#     h,l,s = np.random.uniform(0,1,n), 0.4 + np.random.uniform(0,0.6,n), 0.2 + np.random.uniform(0,0.8,n)
-#     cols = np.stack([colorsys.hls_to_rgb(_h,_l,_s) for _h,_l,_s in zip(h,l,s)],axis=0)
-#     cols[0] = 0
-#     return matplotlib.colors.ListedColormap(cols)
+from glob import glob
+from tqdm import tqdm
+from tifffile import imread
+from csbdeep.utils import Path, download_and_extract_zip_file
 
-# lbl_cmap = random_label_cmap()
-
-
-
-
-
-
-# # Set path for images and labels
-# X = sorted(glob('ground_truth/images/*.tif'))
-# Y = sorted(glob('ground_truth/masks/*.tif'))
-# assert all(Path(x).name==Path(y).name for x,y in zip(X,Y))
-
-# # Load sample image
-# X, Y = X[:1], Y[:1]
-
-# X = list(map(imread,X))
-# Y = list(map(imread,Y))
+from stardist import fill_label_holes, relabel_image_stardist, random_label_cmap
+from stardist.matching import matching_dataset
 
 
-# # Example image
-# i = min(0, len(X))
-# img, lbl = X[i], fill_label_holes(Y[i])
-# assert img.ndim in (2,3)
-# img = img if img.ndim==2 else img[...,:3]
-# # assumed axes ordering of img and lbl is: YX(C)
 
-# plt.figure(figsize=(16,10))
-# plt.subplot(121); plt.imshow(img,cmap='gray');   plt.axis('off'); plt.title('Raw image')
-# plt.subplot(122); plt.imshow(lbl,cmap=lbl_cmap); plt.axis('off'); plt.title('GT labels')
-# # None;
+# Random label cmap
+import matplotlib
+import colorsys
+def random_label_cmap(n=2**16):
+
+    h,l,s = np.random.uniform(0,1,n), 0.4 + np.random.uniform(0,0.6,n), 0.2 + np.random.uniform(0,0.8,n)
+    cols = np.stack([colorsys.hls_to_rgb(_h,_l,_s) for _h,_l,_s in zip(h,l,s)],axis=0)
+    cols[0] = 0
+    return matplotlib.colors.ListedColormap(cols)
+
+lbl_cmap = random_label_cmap()
+
+
+
+
+
+
+# Set path for images and labels
+X = sorted(glob('ground_truth/images/*.tif'))
+Y = sorted(glob('ground_truth/masks/*.tif'))
+assert all(Path(x).name==Path(y).name for x,y in zip(X,Y))
+
+# Load sample image
+X, Y = X[:1], Y[:1]
+
+X = list(map(imread,X))
+Y = list(map(imread,Y))
+
+
+# Example image
+i = min(0, len(X))
+img, lbl = X[i], fill_label_holes(Y[i])
+assert img.ndim in (2,3)
+img = img if img.ndim==2 else img[...,:3]
+# assumed axes ordering of img and lbl is: YX(C)
+
+plt.figure(figsize=(16,10))
+plt.subplot(121); plt.imshow(img,cmap='gray');   plt.axis('off'); plt.title('Raw image')
+plt.subplot(122); plt.imshow(lbl,cmap=lbl_cmap); plt.axis('off'); plt.title('GT labels')
+# None;
 
 
 
